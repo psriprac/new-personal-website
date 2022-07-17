80d4a4b8d2b1a33cedb6e0f86cf17c41b534123a
Patrick Sriprachandr <76898394+psriprac@users.noreply.github.com>
2022-07-17 00:36:26 -0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ import { Link } from "gatsby"
 import MenuPage from "./menu"
 import BackIcon from "../images/Back-Icon.svg"
 import BurgerIcon from "../images/Burger.svg"
+import ActionIcon from "../images/Action-Button.svg"
 
 function Sidebar({ sideBar = false, setSideBar = () => {} }) {
     return (
@@ -60,19 +61,44 @@ const Contact = () => {
                         exit={{ opacity: 0 }}
                         transition={{ style: "spring", bounce: 0, duration: 0.5, delay: sideBar ? 0 : 0.5 }}
                     >
-                        <div>
-                            <h1 className="text-5xl py-8">Coming Soon...</h1>
-                            <Link to="/">
+                        <div className="flex flex-col">
+                        <Link to="/">
                             <motion.div 
                                 id="nextpage" 
-                                className="flex flex-row items-center pt-3 pb-20 gap-4"
+                                className="flex flex-row items-center gap-4"
                                 animate={{ scale: [scale, scale + 1.1, scale + 1] }}
-                                transition={{ duration: 0.5, delay: startAnim ? 0.8 : 0 }} onClick={() => setStartAnim(false)}
+                                transition={{ duration: 0.5, delay: startAnim ? 2 : 0 }} onClick={() => setStartAnim(false)}
                             >
-                                <h1 className="text-4xl">Home</h1>
-                                <BackIcon style={{transform: "rotate(90deg)"}}/>
+                                <h1 className="text-4xl">Back to Home</h1>
+                                <BackIcon style={{transform: "rotate(180deg)"}}/>
                             </motion.div>
                             </Link>
+                            <h1 className="text-5xl py-8">I'd love to hear from you</h1>
+
+                            <form name="contactForm">
+                                <ul>
+                                    <li className="flex flex-col py-1">
+                                        <label htmlFor="name" className="text-2xl">Name:</label>
+                                        <input type="text" id="name" name="user_name" className="bg-slate-200 p-2 rounded"/>
+                                    </li>
+                                    <li className="flex flex-col py-1">
+                                        <label htmlFor="email" className="text-2xl">Email:</label>
+                                        <input type="text" id="email" name="user_email" className="bg-slate-200 p-2 rounded"/>
+                                    </li>
+                                    <li className="flex flex-col py-1">
+                                        <label htmlFor="msg" className="text-2xl">Message:</label>
+                                        <textarea id="msg" name="user_msg" className="bg-slate-200 p-2 rounded resize-none h-36"/>
+                                    </li>
+                                    <li className="flex flex-row-reverse py-1">
+                                        <motion.div
+                                            whileTap={{ scale: 0.9 }}
+                                            whileHover={{ scale: 1.1 }}
+                                        >
+                                            <ActionIcon onClick={() => {document.forms["contactForm"].submit()}} style={{transform: "rotate(270deg)"}}/>
+                                        </motion.div>
+                                    </li>
+                                </ul>
+                            </form>
                         </div>
                     </motion.div>
                 </div>
